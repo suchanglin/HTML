62ca39e7db5e948e7c5dc7f6b87c00b0f0ba4c56
AllySu <18795397123@163.com>
2017-11-07 22:51:52 +0800

    分别添加了注释

--- ajax_htmljsphp/gethint.php
+++ ajax_htmljsphp/gethint.php
@@ -4,7 +4,7 @@
  * @Author: Administrator
  * @Date:   2017-11-07 22:30:35
  * @Last Modified by:   Administrator
- * @Last Modified time: 2017-11-07 22:31:52
+ * @Last Modified time: 2017-11-07 22:51:00
  */
 // Fill up array with names
 $a[]="Anna";
@@ -74,4 +74,15 @@ $response=$hint;
 
 //output the response
 echo $response;
+
+
+// 被 JavaScript 代码调用的服务器页面是一个名为 "gethint.php" 的简单服务器页面。
+// "gethint.php" 中的代码会检查名字数组，然后向客户端返回对应的名字：
+// 如果存在从 JavaScript 送来的文本 (strlen($q) > 0)，则：
+// 1找到与 JavaScript 所传送的字符相匹配的名字
+// 2如果找到多个名字，把所有名字包含在 response 字符串中
+// 3如果没有找到匹配的名字，把 response 设置为 "no suggestion"
+// 4如果找到一个或多个名字，把 response 设置为这些名字
+// 5把 response 发送到 "txtHint" 占位符
+
 ?>
