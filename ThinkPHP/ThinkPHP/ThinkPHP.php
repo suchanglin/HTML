0fe945bd4f6a28055025ca6f60d1d56276c62dec
AllySu <18795397123@163.com>
2017-11-08 20:03:22 +0800

    thinkphp

---
+++ ThinkPHP/ThinkPHP/ThinkPHP.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @Author: Administrator
+ * @Date:   2017-11-08 14:06:21
+ * @Last Modified by:   Administrator
+ * @Last Modified time: 2017-11-08 14:07:06
+ */
+echo "欢迎使用ThinkPHP!";
