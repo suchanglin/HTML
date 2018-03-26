cb81c8c127b4fcd541a86a609be9e7def059dd48
AllySu <18795397123@163.com>
2018-03-26 18:53:54 +0800

    微信开发

--- 慕课网站资源下载/ajax全接触/serverjsonp.php
+++ 慕课网站资源下载/ajax全接触/serverjsonp.php
@@ -1,10 +1,10 @@
 <?php
 //设置页面内容是html编码格式是utf-8
-//header("Content-Type: text/plain;charset=utf-8"); 
-header("Content-Type: application/json;charset=utf-8"); 
-//header("Content-Type: text/xml;charset=utf-8"); 
-//header("Content-Type: text/html;charset=utf-8"); 
-//header("Content-Type: application/javascript;charset=utf-8"); 
+//header("Content-Type: text/plain;charset=utf-8");
+header("Content-Type: application/json;charset=utf-8");
+//header("Content-Type: text/xml;charset=utf-8");
+//header("Content-Type: text/html;charset=utf-8");
+//header("Content-Type: application/javascript;charset=utf-8");
 
 //定义一个多维数组，包含员工的信息，每条员工信息为一个数组
 $staff = array
@@ -25,27 +25,27 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
 
 //通过员工编号搜索员工
 function search(){
-	$jsonp = $_GET["callback"];
+	$jsonp = $_GET["callback"];    //跨域，获取回调函数，必须与客户端属性值jsonp:"callback"相同
 	//检查是否有员工编号的参数
 	//isset检测变量是否设置；empty判断值为否为空
 	//超全局变量 $_GET 和 $_POST 用于收集表单数据
 	if (!isset($_GET["number"]) || empty($_GET["number"])) {
-		echo $jsonp . '({"success":false,"msg":"参数错误"})';
+		echo $jsonp . '({"success":false,"msg":"参数错误"})';     //格式  $jsonp.'(此处为json对象或json数组)'
 		return;
 	}
 	//函数之外声明的变量拥有 Global 作用域，只能在函数以外进行访问。
 	//global 关键词用于访问函数内的全局变量
 	global $staff;
 	//获取number参数
-	$number = $_GET["number"];
+	$number = $_GET["number"];    //客户端传过来的number，用来跟服务端的number比对，是否存在
 	$result = $jsonp . '({"success":false,"msg":"没有找到员工。"})';
-	
+
 	//遍历$staff多维数组，查找key值为number的员工是否存在，如果存在，则修改返回结果
 	foreach ($staff as $value) {
 		if ($value["number"] == $number) {
 			$result = $jsonp . '({"success":true,"msg":"找到员工：员工编号：' . $value["number"] .
-							'，员工姓名：' . $value["name"] . 
-							'，员工性别：' . $value["sex"] . 
+							'，员工姓名：' . $value["name"] .
+							'，员工性别：' . $value["sex"] .
 							'，员工职位：' . $value["job"] . '"})';
 			break;
 		}
@@ -64,7 +64,7 @@ function create(){
 		return;
 	}
 	//TODO: 获取POST表单数据并保存到数据库
-	
+
 	//提示保存成功
 	echo '{"success":true,"msg":"员工：' . $_POST["name"] . ' 信息保存成功！"}';
 }
